00ad4124783b9f3176228ff21e6a22e90488b068
helinkesip <hlnksp4@gmail.com>
2025-07-26 18:17:07 +0300

    dto düzenlendi

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,6 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
-
-class RegisterDto {
-  email: string;
-  password: string;
-}
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('auth')
 export class AuthController {
